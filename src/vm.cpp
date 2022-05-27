7651815b15c9fb45651b7be2da83777a0a3cde82
Silvan Wegmann <silvan.wegmann@hslu.ch>
2022-05-27 11:29:53 +0200

    Add input instruction for KEY word

--- src/vm.cpp
+++ src/vm.cpp
@@ -1,13 +1,17 @@
 #include "vm.h"
+#include <conio.h>
+#include <iostream>
 
 enum class Opcode {
     OP_NOP = 0x0,
+    OP_INPUT = 0x1,
     OP_TERMINATE = 0xFE,
     OP_ILLEGAL = 0xFF
 };
 
 Vm::Vm() {
     memory.fill(static_cast<uint8_t>(Opcode::OP_ILLEGAL));
+    memory[0] = static_cast<uint8_t>(Opcode::OP_INPUT);
 
     // Setup the memory layout
     // +------------+------------+--------------+----------------+-------------+
@@ -20,9 +24,16 @@ Vm::Vm() {
 }
 
 Vm::Result Vm::interpret() {
+    char ch;
+
     while (true) {
         Opcode op = static_cast<Opcode>(fetch_op());
         switch (static_cast<Opcode>(op)) {
+            case Opcode::OP_INPUT:
+                ch = getch();
+                memory[reg_dsp--] = ch;
+                std::cout << ch;
+                break;
             case Opcode::OP_ILLEGAL:
                 return IllegalInstruction;
             case Opcode::OP_TERMINATE:
