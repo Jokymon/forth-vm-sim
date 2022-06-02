ba66ee6d13d73fee30ed9f99ad748add664a74a0
Silvan Wegmann <silvan.wegmann@hslu.ch>
2022-06-02 21:46:33 +0200

    Add comment support to assembler

---
+++ fbuilder/test_parser.py
@@ -0,0 +1,45 @@
+import pathlib
+import pytest
+from lark import Lark
+
+
+@pytest.fixture
+def parser():
+    script_dir = pathlib.Path(__file__).parent
+    lark_grammar_path = script_dir / "grammar.lark"
+
+    grammar = lark_grammar_path.read_text()
+    return Lark(grammar, parser='lalr')
+
+
+def test_codeblock(parser):
+    source = """
+    codeblock
+    end
+    """
+    parser.parse(source)
+
+
+def test_defcode(parser):
+    source = """
+    defcode TEST_CODE
+    end
+    """
+    parser.parse(source)
+
+
+def test_defword(parser):
+    source = """
+    defword TEST_WORD
+    end
+    """
+    parser.parse(source)
+
+
+def test_comments(parser):
+    source = """
+    // comments on lines by themselves
+    codeblock // comment on block start
+        some_op_code // comment on opcode line
+    end // comment on block end
+    """
