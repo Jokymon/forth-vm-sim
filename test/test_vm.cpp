0fae74aaf2752d5ced0155648ccd74078ed78bd7
Silvan Wegmann <silvan.wegmann@hslu.ch>
2022-06-10 12:43:23 +0200

    Remove dummy test

--- test/test_vm.cpp
+++ test/test_vm.cpp
@@ -1,10 +1,6 @@
 #include <catch2/catch_test_macros.hpp>
 #include "vm.h"
 
-TEST_CASE("Generic test", "passing") {
-    REQUIRE( true == true );
-}
-
 TEST_CASE("Memory access", "Loading VM memory through iterator") {
     std::array<uint8_t, 3> testdata = {0x23, 0x53, 0x61};
 
