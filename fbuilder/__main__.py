4f537e1fb3bd24c43cb6586e3f610b220528e705
Silvan Wegmann <silvan.wegmann@hslu.ch>
2022-06-15 09:12:11 +0200

    Add fbuilder output format for creating c-array test vectors

--- fbuilder/__main__.py
+++ fbuilder/__main__.py
@@ -4,7 +4,7 @@ from lark import Lark
 from assembler import VmForthAssembler
 
 
-def compile_forth(input_path, output_path):
+def compile_forth(input_path, output_path, output_format):
     script_dir = pathlib.Path(__file__).parent
     lark_grammar_path = script_dir / "grammar.lark"
 
@@ -13,8 +13,14 @@ def compile_forth(input_path, output_path):
 
     dictionary_source = pathlib.Path(input_path).read_text()
 
-    with open(output_path, "wb") as output_file:
-        output_file.write(lark_parser.parse(dictionary_source))
+    if output_format == "bin":
+        with open(output_path, "wb") as output_file:
+            output_file.write(lark_parser.parse(dictionary_source))
+    else:
+        with open(output_path, "w") as output_file:
+            data = lark_parser.parse(dictionary_source)
+            output = ", ".join(map(hex, data))
+            output_file.write(output)
 
 
 def main():
@@ -23,10 +29,12 @@ def main():
                         help="input file for compilation")
     parser.add_argument('-o', '--output', dest='output', type=str, required=True,
                         help="output file for compiled data")
+    parser.add_argument('-f', '--format', dest='format', choices=['bin', 'carray'], default="bin",
+                        help="output format for the assembled code")
 
     args = parser.parse_args()
 
-    compile_forth(args.input, args.output)
+    compile_forth(args.input, args.output, args.format)
 
 
 if __name__=="__main__":
