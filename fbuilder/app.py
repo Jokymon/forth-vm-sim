86d6aec662f5d18354dcd0a00425d241c4e19891
Silvan Wegmann <silvan.wegmann@hslu.ch>
2023-02-16 09:24:37 +0100

    Add support for fvs-includes

--- fbuilder/app.py
+++ fbuilder/app.py
@@ -1,8 +1,37 @@
 import pathlib
 from lark import Lark
+from lark.lexer import Lexer, LexerState
 from assembler import VmForthAssembler
 from emitter import MachineCodeEmitter, DisassemblyEmitter
 from debug_symbols import WordCollection
+
+
+# Recursive lexer idea copied from https://gist.github.com/MegaIng/c6abba4d9be87473d8d586734f2b39c9
+# and adapted slightly
+class RecursiveLexerThread:
+    def __init__(self, lexer: Lexer, lexer_state: LexerState):
+        self.lexer = lexer
+        self.state_stack = [lexer_state]
+
+    @classmethod
+    def from_text(cls, lexer: 'Lexer', text: str):
+        return cls(lexer, LexerState(text))
+
+    def lex(self, parser_state):
+        while self.state_stack:
+            lexer_state = self.state_stack[-1]
+            lexer = self.lexer.lex(lexer_state, parser_state)
+            try:
+                token = next(lexer)
+            except StopIteration:
+                self.state_stack.pop()  # We are done with this file
+            else:
+                if token.type == "_INCLUDE":
+                    name = token.value.split()[-1]  # get just the string
+                    name = name[1:-1]  # Remove "
+                    include_file = pathlib.Path(name)
+                    self.state_stack.append(LexerState(include_file.read_text()))
+                yield token  # The parser still expects this token either way
 
 
 class Assembler:
@@ -30,7 +59,7 @@ class Assembler:
         script_dir = pathlib.Path(__file__).parent
         lark_grammar_path = script_dir / "grammar.lark"
         grammar = lark_grammar_path.read_text()
-        lark_parser = Lark(grammar, parser='lalr')
+        lark_parser = Lark(grammar, parser='lalr', _plugins={"LexerThread": RecursiveLexerThread})
 
         parse_tree = lark_parser.parse(source_code)
 
