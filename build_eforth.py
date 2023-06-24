a16aa20e24279db418130a16b2771904ae1bd81d
Silvan Wegmann <silvan.wegmann@hslu.ch>
2023-06-24 16:19:18 +0200

    Small improvements in eforth build script

--- build_eforth.py
+++ build_eforth.py
@@ -3,19 +3,25 @@ import sys
 
 
 def main():
-    cp = subprocess.run([sys.executable, "fbuilder", "eforth/eforth_system.fvs", "-o", "eforth/eforth_system.bin", "-fbin", "--sym"], capture_output=True)
-    if cp.returncode!=0:
+    cp = subprocess.run([sys.executable, "fbuilder",
+                         "eforth/eforth_system.fvs", "-o",
+                         "eforth/eforth_system.bin", "-fbin",
+                         "--sym"], capture_output=True)
+    if cp.returncode != 0:
         print(f"Binary generation failed with return code {cp.returncode}")
-        print(cp.stdout)
-        print(cp.stderr)
+        print(cp.stdout.decode("utf-8"))
+        print(cp.stderr.decode("utf-8"))
         return
-    cp = subprocess.run([sys.executable, "fbuilder", "eforth/eforth_system.fvs", "-o", "eforth/eforth_system.S", "-fdisassembly"], capture_output=True)
-    if cp.returncode!=0:
+    cp = subprocess.run([sys.executable, "fbuilder",
+                         "eforth/eforth_system.fvs", "-o",
+                         "eforth/eforth_system.S", "-fdisassembly"],
+                        capture_output=True)
+    if cp.returncode != 0:
         print(f"Disassembly generation failed with return code {cp.returncode}")
-        print(cp.stdout)
-        print(cp.stderr)
+        print(cp.stdout.decode("utf-8"))
+        print(cp.stderr.decode("utf-8"))
         return
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
